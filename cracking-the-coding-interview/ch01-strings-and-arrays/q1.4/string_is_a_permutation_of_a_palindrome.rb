384b255b98aa166f39145e148442c89b16b98b12
aonomike <aonomike@gmail.com>
2020-03-21 06:38:33 +0300

    Run rubocop

--- cracking-the-coding-interview/ch01-strings-and-arrays/q1.4/string_is_a_permutation_of_a_palindrome.rb
+++ cracking-the-coding-interview/ch01-strings-and-arrays/q1.4/string_is_a_permutation_of_a_palindrome.rb
@@ -1,34 +1,37 @@
+# frozen_string_literal: true
+
 ## Palindrome Permutation:
-	#Given a string, write a function to check if it is a permutation of a palindrome.
-	#A palindrome is a word or phrase that is the same forwards and backwards.
-	#A permutation is a rearrangement of letters. 
-	#The palindrome does not need to be limited to just dictionary words.
-	#Input: "Tact Coa"
-	#Output: True (permutations: "taco cat", "atco eta"
+# Given a string, write a function to check if it is a permutation of a palindrome.
+# A palindrome is a word or phrase that is the same forwards and backwards.
+# A permutation is a rearrangement of letters.
+# The palindrome does not need to be limited to just dictionary words.
+# Input: "Tact Coa"
+# Output: True (permutations: "taco cat", "atco eta"
 
 def is_a_permutation_of_a_palindrome(s) # lengh(s) = n
-    character_hash = {}
-    s.downcase! # n loops
-    s.tr(' ', '').split(//).each do |char| # 3*n loops
-        if character_hash.key?(char) # constant
-            character_hash[char] += 1 # constant
-        else
-            character_hash[char] = 1 # constant
-        end
+  character_hash = {}
+  s.downcase! # n loops
+  s.tr(' ', '').split(//).each do |char| # 3*n loops
+    if character_hash.key?(char) # constant
+      character_hash[char] += 1 # constant
+    else
+      character_hash[char] = 1 # constant
     end
-    odd = 0
+  end
+  odd = 0
 
-    character_hash.values.each do |val| # less or equal n loops
-        if val % 2 == 1 # constant
-            odd += 1  # constant
-        end
+  character_hash.values.each do |val| # less or equal n loops
+    if val.odd? # constant
+      odd += 1 # constant
     end
-    p character_hash
-    
-    if odd > 1 # constant
-        return false # constant
-    end
-    return true
+  end
+  p character_hash
+
+  if odd > 1 # constant
+    return false # constant
+  end
+
+  true
 end
 
 # 4*n loops => it's linear (length of the string)
@@ -40,5 +43,3 @@ p is_a_permutation_of_a_palindrome('aab bba')
 p is_a_permutation_of_a_palindrome('Tact Coa')
 
 # Test : if there are more than two charecters with an odd number of instance => not a permutation of a palindrom
-
-
