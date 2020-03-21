848c5c03590759c9fab58e9f2aa57d787ad975f2
aonomike <aonomike@gmail.com>
2020-03-21 06:32:15 +0300

    Add question as comment

--- cracking-the-coding-interview/ch01-strings-and-arrays/q1.4/string_is_a_permutation_of_a_palindrome.rb
+++ cracking-the-coding-interview/ch01-strings-and-arrays/q1.4/string_is_a_permutation_of_a_palindrome.rb
@@ -1,4 +1,10 @@
-# Taco Cat
+## Palindrome Permutation:
+	#Given a string, write a function to check if it is a permutation of a palindrome.
+	#A palindrome is a word or phrase that is the same forwards and backwards.
+	#A permutation is a rearrangement of letters. 
+	#The palindrome does not need to be limited to just dictionary words.
+	#Input: "Tact Coa"
+	#Output: True (permutations: "taco cat", "atco eta"
 
 def is_a_permutation_of_a_palindrome(s) # lengh(s) = n
     character_hash = {}
