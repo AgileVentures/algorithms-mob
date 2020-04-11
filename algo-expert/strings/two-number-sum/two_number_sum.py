da69add0aeb98b4f39d274d7e1fa00dc9b553984
aonomike <aonomike@gmail.com>
2020-04-12 02:48:33 +0300

    Add question

--- algo-expert/strings/two-number-sum/two_number_sum.py
+++ algo-expert/strings/two-number-sum/two_number_sum.py
@@ -1,3 +1,15 @@
+#  write a function that takes a non-empty array of distinct integers
+# and an integer representing a target sum. If any two number in the input array adds up to the target sum,
+# the function should return them in an array, in any order.
+# If no two numbers sum up to the target sum, the function should return an empty array.
+# You can assume that there will be at most one pair of numbers that add up to the target sum.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1]
 class TwoNumberSum:
     def __init__(self):
         pass
